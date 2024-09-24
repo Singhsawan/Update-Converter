64f8159158c1f65b1332b249dc3b30b989720c3a
Akhand Bharat <125857863+Singhsawan@users.noreply.github.com>
2024-09-24 23:03:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ API_ID = int(os.environ.get("API_ID", 21748181))
 API_HASH = os.environ.get("API_HASH", 'b1d962414e186e0778911f3183feac33')
 BOT_TOKEN = os.environ.get("BOT_TOKEN", '6806119336:AAHPgsNAm6muevQanFDVxqKRQ-JFHlsrx5c')
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else []
-OWNER_ID = int(os.environ.get("OWNER_ID", "5651594253"))
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "zxlink")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SawanSingh:sawansingh@cluster0.h2prshr.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5651594253 6201745002"))
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "ziplinker")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://kandudon598:sawan@cluster0.z3nez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # Optionnal variables
-BROADCAST_AS_COPY = is_enabled((os.environ.get('BROADCAST_AS_COPY', "False")), False)
+BROADCAST_AS_COPY = is_enabled((os.environ.get('BROADCAST_AS_COPY', "True")), True)
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '')
 BIN_CHANNEL = os.environ.get("BIN_CHANNEL", -1001765670990)
 #  Replit Config
@@ -25,7 +25,7 @@ REPLIT_USERNAME = os.environ.get("REPLIT_USERNAME", None)
 REPLIT_APP_NAME = os.environ.get("REPLIT_APP_NAME", None)
 REPLIT = f"https://{REPLIT_APP_NAME.lower()}.{REPLIT_USERNAME}.repl.co" if REPLIT_APP_NAME and REPLIT_USERNAME else False
 PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "300"))
-BASE_SITE = "zxlink.in"
+BASE_SITE = "Tryshort.in"
 
 if OWNER_ID not in ADMINS:
     ADMINS.append(OWNER_ID)
